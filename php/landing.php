23e4d825fc56c41a72c9d43612da4197dce9f5de
Jereme <jereme.daniels@gmail.com>
2019-02-27 13:29:38 -0500

    landing page

--- php/landing.php
+++ php/landing.php
@@ -4,11 +4,11 @@
 ?>
     <div id="landing">
         <div id="landing-header">
-            <img src="./media/trucksrear.png" alt="trucks lined up" id="trucks"/>
+            <img src="./media/trucksrear.png" alt="trucks lined up" class="landing-imgs"/>
             <h3>Serving the High Country of Western NC and Eastern TN since 1992</h3>
             <img src="./media/mapsquare.png" alt="service area" id="phone-map" />
-            
         </div>
+
         <div class="landing-div">
             <h3>Your only <strong>locally</strong> owned and
             operated full service propane company based in Avery
@@ -16,6 +16,26 @@
             know from your community.</h3>
         </div>
         
+        <div >
+            <img src="./media/tanks2000.png" alt="large lp tanks" class="landing-imgs"/>
+        </div>
+
+        <div class="landing-div">
+            <h3>Call us or click the link today to become a customer 
+                or to let us know how we can serve you better!
+            </h3>
+            <a href="/brp/php/contact.php">Connect with us here!</a>
+
+        </div>
+        <div class="landing-div">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1030.5687581255377!2d-81.93380285487038!3d36.11588108174933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x885093c98a5b7f61%3A0x29dc06deeba0ad8c!2s14+Turbyfill+Rd%2C+Newland%2C+NC+28657!5e0!3m2!1sen!2sus!4v1550507219401" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+        
+        <div class="landing-div" id="hours">
+                <h2>Hours of operation:</h2>
+                <h3>7:30 - 4 Monday - Friday</h3>
+                <h3>24/365 Emergency Service</h3>
+        </div>
     </div>
 
 <?php
